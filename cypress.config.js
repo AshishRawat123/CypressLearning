c6987171db8782ad47070db343d63bfacfe25a99
AshishRawat123 <ashishrawat1911@gmail.com>
2024-09-13 19:33:43 +0530

    added code for video recording on test failure

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,27 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs')
 
 module.exports = defineConfig({
   e2e: {
+    video: true,  // Enable video recording
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('@cypress/grep/src/plugin')(config);
+
+      // Video on Failure mechanism
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      });
+
       return config;
     },
     "experimentalStudio": true
