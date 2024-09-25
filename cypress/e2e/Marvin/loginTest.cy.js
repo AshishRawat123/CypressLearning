45d414e88976289a632c428634b6ff96832cf031
AshishRawat123 <ashishrawat1911@gmail.com>
2024-09-25 14:09:09 +0530

    fix for login Test

--- cypress/e2e/Marvin/loginTest.cy.js
+++ cypress/e2e/Marvin/loginTest.cy.js
@@ -15,36 +15,32 @@ describe('Login with valid user Details',()=>{
     })
 
     beforeEach('In login Page',()=>{
-        // Login.openPage();
         cy.visit('https://www.saucedemo.com');
     })
 
 
     validDetails.forEach(userdetail => {
-        it(`should Login with username : ${userdetail.username} and password : ${userdetail.password}`,()=>{
+        it(`should Login with username : ${userdetail.username} and password : ${userdetail.password} and Logout`,()=>{
 
             Login.loginwithUserDetails(userdetail.username, userdetail.password);
             Inventory.elements.menuButton().should('be.visible');
             cy.url().should('include', '/inventory.html');
-
+            Inventory.elements.menuButton().click();
+            Inventory.elements.logoutButton().click();
+            cy.url().should('not.include', '/inventory.html');
         })
     });
 
     it('should not Login with invalid user Details and validate error message',()=>{
+
         Login.loginwithUserDetails(invalidUserJson.username, invalidUserJson.password);
         Login.elements.errorNotification().should('not.have.value', 'Epic sadface: Sorry, this user has been locked out.')
 
     })
 
-    afterEach('Clear Cookie',()=>{
-        cy.getAllCookies().then((cookies) => {
-            cy.log(cookies.length);
+    // afterEach('Clear Cookie',()=>{
+    //     cy.clearCookies();
 
-        })
-        cy.clearCookies();
-        cy.clearAllSessionStorage();
-        cy.clearAllSessionStorage()
-        cy.wait(2000);
-    })
+    // })
 
 })
